c0f98812b914b7cf9b0082794f02d4702a221909
Pronob Mozumder <pronobmozumder.info@gmail.com>
2020-10-03 15:46:26 +0600

    day 2

---
+++ com_lab2_1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int j =0;
+    char c[100];
+    int i,f=0,len;
+
+    scanf("%[^\n]", &c);
+
+    len = strlen(c);
+
+    if (len == 0)
+    {
+        printf("You did not entered any number.");
+    }
+    else
+    {
+        int k=0;
+        i= 1;
+        printf("\nExpected Output : \n");
+        while(i<=len)
+        {
+
+
+            if ((c[i-1] == '\\' ) && (c[i] == 'n'))
+            {
+                i+=2;
+                printf("\n");
+            }
+
+
+            else if ((c[i-1] =='\\') && (c[i] =='\\'))
+            {
+                f = 1;
+            }
+            else if ((c[i-1] =='\\') && (c[i] =='*'))
+            {
+                f = 1;
+            }
+            else if ((c[i-1] =='*') && (c[i] =='\\'))
+            {
+                i+=2;
+                f = 0;
+            }
+
+            if(f == 0)
+            {
+                printf("%c",c[i-1]);
+            }
+
+            i++;
+        }
+
+    }
+    return 0;
+}
