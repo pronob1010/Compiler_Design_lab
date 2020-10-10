d532f8462c97d0323d5979a639b5be11a5720a91
Pronob Mozumder <pronobmozumder.info@gmail.com>
2020-10-10 16:56:58 +0600

    Extra Task

---
+++ compiler_day3_extra_task.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000000];
+    int f, i, fc=-1, len;
+
+    scanf("%[^\n]", &s);
+
+    len = strlen(s);
+
+    f = 0;
+    int st = 0;
+
+    for(i = 0; i<len; i++)
+    {
+        if(s[i] =='.')
+        {
+            f = 1;
+        }
+
+        if(f == 1)
+        {
+            fc++;
+        }
+    }
+
+    if(f==0)
+    {
+        printf("Integer");
+    }
+    else if(fc>0 && fc<=6)
+    {
+        printf("Float");
+    }
+    else if(fc>6 && fc<=15)
+    {
+        printf("Double");
+    }
+    else if(fc>15 && fc<=19)
+    {
+        printf("Long Double");
+    }
+
+    return 0;
+}
+
